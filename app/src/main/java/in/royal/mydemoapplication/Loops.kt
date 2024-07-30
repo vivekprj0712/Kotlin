4fb1e9b5540e9c607620995f59e2715f7309aabc
vivekprj0712 <prajapativivek949@gmail.com>
2024-07-30 11:10:32 +0530

    Loops

---
+++ app/src/main/java/in/royal/mydemoapplication/Loops.kt
@@ -0,0 +1,107 @@
+package `in`.royal.mydemoapplication
+
+//import android.os.Bundle
+//import androidx.activity.ComponentActivity
+//import androidx.activity.compose.setContent
+//import androidx.activity.enableEdgeToEdge
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.material3.Scaffold
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.tooling.preview.Preview
+//import `in`.royal.mydemoapplication.ui.theme.MyDemoApplicationTheme
+//
+//class Loops : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContent {
+//            MyDemoApplicationTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    MyDemoApplicationTheme {
+//        Greeting("Android")
+//    }
+//}
+
+fun main() {
+
+// Loops:-
+//    1)While
+//    2)Do-While
+//    3)For
+
+//    1)While:-
+    var count: Int = 1
+    var num : Int = 7
+//    while (count <= 10) {
+//        println("$num * $count = " + (num*count))  // String Templating
+//        count++
+//    }
+
+
+//    2)Do-While:-
+//    do{
+//        println("$num * $count = " + num*count)
+//        count++
+//    }while (count <=10)
+
+//    3)For:-
+
+//    Using Range:-
+//    for(i in 1..10){
+//        println(i)
+//    }
+
+//    Using Range + Step
+//    for(i in 1..10 step 2){
+//        println(i)
+//    }
+
+//    using Until
+//    for (i in 1 until 11)
+//    {
+//        println(i)
+//    }
+
+//    using Until + Range
+//    for (i in 1 until 10 step 2){
+//        println(i)
+//    }
+
+//    Using DownTO
+//    for (i in 10 downTo 1){
+//        println(i)
+//    }
+
+//    Using DownTO + Step
+//    for (i in 10 downTo 1 step 2){
+//        println(i)
+//    }
+
+
+
+
+}
