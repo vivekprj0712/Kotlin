95efbeb57e584407e8043994782be26a1345a52d
vivekprj0712 <prajapativivek949@gmail.com>
2024-07-30 11:29:17 +0530

    ConditionStatement

---
+++ app/src/main/java/in/royal/mydemoapplication/ConditionStatement.kt
@@ -0,0 +1,43 @@
+package `in`.royal.mydemoapplication
+
+fun main() {
+    var no1 = 10
+    var no2 = 11
+
+//    1) if-else
+//    if (no1 % 2 == 0){
+//        println("Even")
+//    }else{
+//        println("Odd")
+//    }
+
+
+//    2) if-else if-else
+//    if (no1 == no2){
+//        println("Both Are Equal")
+//    }else if(no1 > no2){
+//        println("$no1 is bigger then $no2")
+//    }else{
+//        println("$no2 is bigger then $no1")
+//    }
+
+//    3) Other Way
+//    var result = if (no1 == no2){
+//        "Both Are Equal"
+//    }else if(no1 > no2){
+//        "$no1 is bigger then $no2"
+//    }else{
+//        "$no2 is bigger then $no1"
+//    }
+//    println(result)
+
+//    4) Expression Way in if-else:-
+//    var result = if (no2 %  2 == 0) "Even" else "Odd"
+//    println(result)
+
+//    5) Expression Way in if-else if-else:-
+//    var result = if (no1 == no2)  "Both Are Equal" else if (no1 > no2)  "$no1 is bigger then $no2"  else  "$no2 is bigger then $no1"
+//    println(result)
+
+
+}
