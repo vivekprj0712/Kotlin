c0fb846e1f877e0249a77b7415af793b55393755
vivekprj0712 <prajapativivek949@gmail.com>
2024-07-30 11:20:28 +0530

    BasicConcept

---
+++ app/src/main/java/in/royal/mydemoapplication/BasicConcept.kt
@@ -0,0 +1,41 @@
+package `in`.royal.mydemoapplication
+
+//Multi Line Comment
+/*
+    Multi Line Comment
+*/
+
+// Print Hello World and Name in kotlin
+// fun main() is Entry Point
+//fun main(){
+//    println("Hello World") // print msg and cursor got to the next line
+//    print("Vivek ") // print msg and cursor not got to the next line
+//    print("Prajapati")
+//}
+
+fun main(){
+
+//    println("Enter No1 :")
+//    val no1 = readln() //fetch value from user
+//    val no2 = no1.toInt()
+//    val multiplier = 10
+//    println("Multiplier of ${no1} * ${multiplier} : ${no2 * multiplier}")
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
