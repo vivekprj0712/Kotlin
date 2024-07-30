9b5ab95784e7961b5959b361f7deb221a857f6fa
vivekprj0712 <prajapativivek949@gmail.com>
2024-07-30 11:27:59 +0530

    Variables & DataTypes

--- app/src/main/java/in/royal/mydemoapplication/VariablesAndDataTypes.kt
+++ app/src/main/java/in/royal/mydemoapplication/VariablesAndDataTypes.kt
@@ -1,52 +1,5 @@
 package `in`.royal.mydemoapplication
 
-//import android.os.Bundle
-//import androidx.activity.ComponentActivity
-//import androidx.activity.compose.setContent
-//import androidx.activity.enableEdgeToEdge
-//import androidx.compose.foundation.layout.fillMaxSize
-//import androidx.compose.foundation.layout.padding
-//import androidx.compose.material3.Scaffold
-//import androidx.compose.material3.Text
-//import androidx.compose.runtime.Composable
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.tooling.preview.Preview
-//import `in`.royal.mydemoapplication.ui.theme.MyDemoApplicationTheme
-//
-//class VariablesAndDataTypes : ComponentActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//        setContent {
-//            MyDemoApplicationTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    MyDemoApplicationTheme {
-//        Greeting("Android")
-//    }
-//}
-
-
 fun main() {
 //    var :- var can be reassigned
     var no = 5
